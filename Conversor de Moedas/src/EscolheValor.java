956464a46f207a16adbe9a232340cae060234362
Maurilio <fmaurilio6@gmail.com>
2023-03-16 09:26:41 -0300

    Create EscolheValor.java

---
+++ Conversor de Moedas/src/EscolheValor.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JOptionPane;
+
+public class EscolheValor {
+
+    private final String valor;
+    public EscolheValor() {
+        valor = JOptionPane.showInputDialog(null, "Insira um valor: ");
+    }
+
+    public double getValor() throws NumberFormatException {
+        return Double.parseDouble(this.valor);
+    }
+}
